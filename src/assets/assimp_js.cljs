233bc1364ca7507cff8fad345d8c149da742580e
keychera <kyogatama@gmail.com>
2025-11-22 00:31:12 +0900

    exploration

--- src/assets/assimp_js.cljs
+++ src/assets/assimp_js.cljs
@@ -13,7 +13,13 @@
                     js/Promise.all)
                (.then
                 (fn [arrayBuffers]
-                  (js/console.log arrayBuffers)
-                  [ajs])))))))
+                  (let [ajs-file-list (new (.-FileList ajs))]
+                    (dotimes [i (count files)]
+                      (.AddFile ajs-file-list (nth files i) (js/Uint8Array. (aget arrayBuffers i))))
+                    (def hmm (.ConvertFileList ajs ajs-file-list "assjson"))))))))))
 
-  hmm)
+  (js->clj (->> (.GetFile hmm 0) ;; file 1 will throw error
+                (.GetContent)
+                (.decode (js/TextDecoder.))
+                (js/JSON.parse))
+           :keywordize-keys true))
