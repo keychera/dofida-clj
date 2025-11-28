54ab44200c4b3c499bc7c5237aa07f03b7afe8d1
keychera <kyogatama@gmail.com>
2025-11-29 01:18:33 +0900

    geotiff loaded, tif read, but it loads incorrectly hmm

--- resources/public/assets/models/note.md
+++ resources/public/assets/models/note.md
@@ -3,3 +3,6 @@
 - moon.glb
     - created in blender following this https://www.youtube.com/watch?v=6G2Rk9eB4uU
     - export to .glb/gltf 2.0
+
+- ldem_4_uint.tif (gltf doesnt support displacement apparently)
+    - from nasa web
--- src/minusone/rules/model/assimp_js.cljs
+++ src/minusone/rules/model/assimp_js.cljs
@@ -1,6 +1,7 @@
 (ns minusone.rules.model.assimp-js
   (:require
    ["assimpjs" :as assimpjs]
+   ["geotiff" :as geotiff]
    [engine.math :as m-ext]
    [engine.sugar :refer [f32-arr]]
    [minusone.rules.gl.magic :as gl.magic :refer [gl-incantation]]
@@ -11,7 +12,9 @@
    [thi.ng.geom.matrix :as mat]
    [thi.ng.geom.quaternion :as q]
    [thi.ng.geom.vector :as v]
-   [thi.ng.math.core :as m]))
+   [thi.ng.math.core :as m]
+   [cljs.core.async :refer [go]]
+   [cljs.core.async.interop :refer-macros [<p!]]))
 
 (defonce canvas (js/document.querySelector "canvas"))
 (defonce gl-context (.getContext canvas "webgl2" (clj->js {:premultipliedAlpha false})))
@@ -133,28 +136,92 @@
    (-> gltf-json :images first :uri)
    (fn [bitmap-data width height]
      #_{:clj-kondo/ignore [:inline-def]}
-     (def the-texture (texture/texture-incantation game bitmap-data width height 1))))
+     (def the-texture (texture/texture-incantation game bitmap-data width height 0))))
+
+  (go
+    (let [tiff   (<p! (geotiff/fromUrl "assets/models/ldem_4_uint.tif"))
+          image  (<p! (.getImage tiff))
+          rgb    (<p! (.readRGB image))
+          width  (.getWidth image)
+          height (.getHeight image)]
+      (println "ldem" (type rgb) width height)
+      (def the-ldem-tex (texture/texture-incantation game rgb width height 1))))
 
   (def default-gltf-shader
     (let [vert   {:precision  "mediump float"
                   :inputs     '{POSITION   vec3
                                 NORMAL     vec3
                                 TEXCOORD_0 vec2}
-                  :outputs    '{normal vec3
+                  :outputs    '{vpoint vec3
+                                normal vec3
                                 uv vec2}
                   :uniforms   '{u_mvp mat4}
                   :signatures '{main ([] void)}
                   :functions  '{main ([]
                                       (= gl_Position (* u_mvp (vec4 POSITION "1.0")))
+                                      (= vpoint POSITION)
                                       (= normal NORMAL)
                                       (= uv TEXCOORD_0))}}
+          ;; frag from https://clojurecivitas.github.io/opengl_visualization/main.html
           frag   {:precision  "mediump float"
-                  :inputs     '{normal vec3
+                  :inputs     '{vpoint vec3
+                                normal vec3
                                 uv vec2}
                   :outputs    '{o_color vec4}
-                  :uniforms   '{u_mat sampler2D}
-                  :signatures '{main ([] void)}
-                  :functions  '{main ([] (= o_color (texture u_mat uv)))}}]
+                  :uniforms   '{u_light_pos vec3
+                                u_light_ambient float
+                                u_light_diffuse float
+                                u_resolution float
+                                u_mat sampler2D
+                                u_ldem sampler2D}
+                  :signatures '{ortho_vec ([vec3] vec3)
+                                oriented_matrix ([vec3] mat3)
+                                lonlat ([vec3] vec2)
+                                color ([vec2] vec3)
+                                elevation ([vec3] float)
+                                normal_fn ([mat3 vec3] vec3)
+                                main ([] void)}
+                  :functions  '{ortho_vec
+                                ([n] (=vec3 b (vec3 0))
+                                     ("if (abs(n.x) <= abs(n.y))"
+                                      ("if (abs(n.x) <= abs(n.z))"
+                                       (= b (vec3 1 0 0))
+                                       (= b (vec3 0 0 1))))
+                                     ("else"
+                                      ("if (abs(n.y) <= abs(n.z))"
+                                       (= b (vec3 0 1 0))
+                                       (= b (vec3 0 0 1))))
+                                     (normalize (cross n b)))
+
+                                oriented_matrix
+                                ([n] (=vec3 o1 (ortho_vec n))
+                                     (=vec3 o2 (cross n o1))
+                                     (mat3 n o1 o2))
+
+                                lonlat
+                                ([p] (=float lon "atan(p.x, -p.z) / (2.0 * 3.1415926535897932384626433832795) + 0.5")
+                                     (=float lat "atan(p.y, length(p.xz)) / 3.1415926535897932384626433832795 + 0.5")
+                                     (vec2 lon lat))
+
+                                color ([lonlat] (.rgb (texture u_mat lonlat)))
+
+                                elevation ([p] (.r (texture u_ldem (lonlat p))))
+
+                                normal_fn
+                                ([horizon p]
+                                 (=vec3 pl (+ p (* horizon (vec3 0 -1 0) u_resolution)))
+                                 (=vec3 pr (+ p (* horizon (vec3 0 1 0) u_resolution)))
+                                 (=vec3 pu (+ p (* horizon (vec3 0 0 -1) u_resolution)))
+                                 (=vec3 pd (+ p (* horizon (vec3 0 0 1) u_resolution)))
+                                 (=vec3 u (* horizon (vec3 (- (elevation pr) (elevation pl)) (* "2.0" u_resolution) 0)))
+                                 (=vec3 v (* horizon (vec3 (- (elevation pd) (elevation pu)) 0 (* "2.0" u_resolution))))
+                                 (normalize (cross u v)))
+
+                                main
+                                ([]
+                                 (=mat3 horizon (oriented_matrix (normalize vpoint)))
+                                 (=float phong (+ u_light_ambient (* u_light_diffuse (max "0.0" (dot u_light_pos (normal_fn horizon vpoint))))))
+                                 (= o_color (vec4 (* (.rgb (color (lonlat vpoint))) phong) "1.0")))}}]
       {:esse-id :DEFAULT-GLTF-SHADER
        :program-data (shader/create-program game vert frag)}))
 
@@ -165,60 +232,80 @@
                     [default-gltf-shader]
                     (gltf-magic gltf-json result-bin)))
 
-  (let [indices   (let [mesh      (some-> gltf-json :meshes first)
-                        accessors (some-> gltf-json :accessors)
-                        indices   (some-> mesh :primitives first :indices)]
-                    (get accessors indices))
-        program   (-> default-gltf-shader :program-data :program)
-        uni-loc   (-> default-gltf-shader :program-data :uni-locs)
-        u_mvp     (get uni-loc 'u_mvp)
-        u_mat     (get uni-loc 'u_mat)
-        vao       (nth (first summons) 2)
+  (let [indices         (let [mesh      (some-> gltf-json :meshes first)
+                              accessors (some-> gltf-json :accessors)
+                              indices   (some-> mesh :primitives first :indices)]
+                          (get accessors indices))
+        program         (-> default-gltf-shader :program-data :program)
+        uni-loc         (-> default-gltf-shader :program-data :uni-locs)
+        u_mvp           (get uni-loc 'u_mvp)
 
-        position  (v/vec3 0.0 0.0 3.0)
-        front     (v/vec3 0.0 0.0 -1.0)
-        up        (v/vec3 0.0 1.0 0.0)
-        view      (mat/look-at position (m/+ position front) up)
+        u_light_pos     (get uni-loc 'u_light_pos)
+        u_light_ambient (get uni-loc 'u_light_ambient)
+        u_light_diffuse (get uni-loc 'u_light_diffuse)
+        u_resolution    (get uni-loc 'u_resolution)
+        u_mat           (get uni-loc 'u_mat)
+        u_ldem          (get uni-loc 'u_ldem)
 
-        fov       45.0
-        aspect    (/ width height)
-        project   (mat/perspective fov aspect 0.1 100)
+        vao             (nth (first summons) 2)
 
-        loop-fn
-        (fn [{:keys [total]}]
-          (let [tt        (* 0.002 total)
-                trans-mat (m-ext/translation-mat 0.0 0.0 0.0)
-                rot-mat   (g/as-matrix (q/quat-from-axis-angle
-                                        (v/vec3 0.2 1.0 0.2)
-                                        (m/radians (* tt 25.0))))
-                scale-mat (m-ext/scaling-mat 1.0)
+        position        (v/vec3 0.0 0.0 3.0)
+        front           (v/vec3 0.0 0.0 -1.0)
+        up              (v/vec3 0.0 1.0 0.0)
+        view            (mat/look-at position (m/+ position front) up)
 
-                model     (reduce m/* [trans-mat rot-mat scale-mat])
+        fov             45.0
+        aspect          (/ width height)
+        project         (mat/perspective fov aspect 0.1 100)
+        [^float lx
+         ^float ly
+         ^float lz]     (m/normalize (v/vec3 -1.0 0.0 -1.0))
 
-                mvp       (reduce m/* [project view model])
-                mvp       (f32-arr (vec mvp))]
-            (gl game clear (bit-or (gl game COLOR_BUFFER_BIT) (gl game DEPTH_BUFFER_BIT)))
-            (gl game blendFunc (gl game SRC_ALPHA) (gl game ONE_MINUS_SRC_ALPHA))
-            (gl game viewport 0 0 width height)
+        loop-fn         (fn [{:keys [total]}]
+                          (let [tt        (* 0.002 total)
+                                trans-mat (m-ext/translation-mat 0.0 0.0 0.0)
+                                rot-mat   (g/as-matrix (q/quat-from-axis-angle
+                                                        (v/vec3 0.2 1.0 0.2)
+                                                        (m/radians (* tt 50.0))))
+                                scale-mat (m-ext/scaling-mat 1.0)
 
-            (gl game useProgram program)
-            (gl game bindVertexArray vao)
-            (gl game uniformMatrix4fv u_mvp false mvp)
+                                model     (reduce m/* [trans-mat rot-mat scale-mat])
 
-            (let [{:keys [tex-unit texture]} the-texture]
-              (gl game activeTexture (+ (gl game TEXTURE0) tex-unit))
-              (gl game bindTexture (gl game TEXTURE_2D) texture)
-              (gl game uniform1i u_mat tex-unit))
+                                mvp       (reduce m/* [project view model])
+                                mvp       (f32-arr (vec mvp))]
+                            (gl game clear (bit-or (gl game COLOR_BUFFER_BIT) (gl game DEPTH_BUFFER_BIT)))
+                            (gl game blendFunc (gl game SRC_ALPHA) (gl game ONE_MINUS_SRC_ALPHA))
+                            (gl game viewport 0 0 width height)
 
-            (gl game drawElements
-                (gl game TRIANGLES)
-                (:count indices)
-                (:componentType indices)
-                0)))]
+                            (gl game useProgram program)
+                            (gl game bindVertexArray vao)
+                            (gl game uniformMatrix4fv u_mvp false mvp)
+
+                            (gl game uniform3f u_light_pos lx ly lz)
+                            (gl game uniform1f u_light_ambient 0.0)
+                            (gl game uniform1f u_light_diffuse 1.6)
+                            (gl game uniform1f u_resolution (/ (* 2.0 Math/PI 1737.4) 1440)) ;; manual radius ldem-width    
+
+                            (let [{:keys [tex-unit texture]} the-texture]
+                              (gl game activeTexture (+ (gl game TEXTURE0) tex-unit))
+                              (gl game bindTexture (gl game TEXTURE_2D) texture)
+                              (gl game uniform1i u_mat tex-unit))
+
+                            (let [{:keys [tex-unit texture]} the-ldem-tex]
+                              (gl game activeTexture (+ (gl game TEXTURE0) tex-unit))
+                              (gl game bindTexture (gl game TEXTURE_2D) texture)
+                              (gl game uniform1i u_ldem tex-unit))
+
+                            (gl game drawElements
+                                (gl game TRIANGLES)
+                                (:count indices)
+                                (:componentType indices)
+                                0)))]
     (gl game enable (gl game DEPTH_TEST)) ;; THIS HOLY MOLY, I THOUGHT MY UV IS ALL WRONG!!!!
     (limited-game-loop
      loop-fn
-     {:total (js/performance.now) :delta 0}
+     {:total (js/performance.now)
+      :delta 0}
      5000))
 
   :-)
