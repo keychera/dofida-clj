d111c69fcb5d82f5bd8200b657c76fc5ca5ed7d1
keychera <kyogatama@gmail.com>
2025-12-26 01:32:28 +0900

    try gloss

---
+++ src/minustwo/model/pmx_parser.clj
@@ -0,0 +1,46 @@
+(ns minustwo.model.pmx-parser
+  (:require
+   [clojure.java.io :as io]
+   [gloss.core :as g :refer [finite-frame string]]
+   [gloss.core.structure :refer [compile-frame]]
+   [gloss.io :as gio]))
+
+
+;; https://gist.github.com/felixjones/f8a06bd48f9da9a4539f/b3944390bd935f48ddf72dd2fc058ffe87c10708
+
+;; types
+(g/defcodec fr [:int32 (string :utf-8 :length 10) :int32])
+
+(-> (gio/encode fr [1 "1234567890" 3])
+    (gio/contiguous))
+(gio/decode fr *1)
+
+(defn file->bytes [file]
+  (with-open [xin (io/input-stream file)
+              xout (java.io.ByteArrayOutputStream.)]
+    (io/copy xin xout)
+    (.toByteArray xout)))
+
+(let [frame      [:magic               (string :utf-8 :length 4)
+                  :version             :float32-le
+                  :header-size         :byte
+                  :encoding            :byte
+                  :additional-uv       :byte
+                  :vertex-index-size   :byte
+                  :texture-index-size  :byte
+                  :material-index-size :byte
+                  :bone-index-size     :byte
+                  :morph-index-size    :byte
+                  :rigid-index-size    :byte
+                  :local-name          (finite-frame :int32-le (string :utf-16LE))
+                  :global-name         (finite-frame :int32-le (string :utf-16LE))
+                  :local-comment       (finite-frame :int32-le (string :utf-16LE))
+                  :global-comment      (finite-frame :int32-le (string :utf-16LE))]
+      pmx-codec  (compile-frame frame)
+      model-path "public/assets/models/SilverWolf/SilverWolf.pmx"
+      pmx-byte   (file->bytes (io/file (io/resource model-path)))]
+  (gio/decode pmx-codec pmx-byte false))
+
+;; other refs;
+;; https://gist.github.com/ulrikdamm/8274171
+;; https://github.com/hirakuni45/glfw3_app/blob/master/glfw3_app/docs/PMX_spec.txt
