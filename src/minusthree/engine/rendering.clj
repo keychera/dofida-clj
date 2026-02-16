6fefc509f52344a3c6ec005e6e48aedc986d7c43
keychera <kyogatama@gmail.com>
2026-02-17 00:11:36 +0900

    imgui on its own abode

--- src/minusthree/engine/rendering.clj
+++ src/minusthree/engine/rendering.clj
@@ -1,10 +1,8 @@
 (ns minusthree.engine.rendering
   (:require
-   [fastmath.matrix :refer [mat->float-array]]
    [fastmath.vector :as v]
-   [minusthree.engine.gui.fps-panel :as fps-panel]
-   [minusthree.engine.math :refer [look-at perspective]]
    [minusthree.engine.offscreen :as offscreen]
+   [minusthree.engine.rendering.imgui :as imgui]
    [minusthree.engine.world :as world]
    [minusthree.model.model-rendering :as model-rendering]
    [minustwo.gl.constants :refer [GL_BLEND GL_COLOR_BUFFER_BIT GL_CULL_FACE
@@ -14,34 +12,9 @@
    [minustwo.gl.macros :refer [lwjgl] :rename {lwjgl gl}]
    [odoyle.rules :as o])
   (:import
-   (imgui ImGui)
-   (imgui.extension.imguizmo ImGuizmo)
-   (imgui.flag ImGuiConfigFlags ImGuiWindowFlags)
-   (imgui.gl3 ImGuiImplGl3)
-   (imgui.glfw ImGuiImplGlfw)
    [org.lwjgl.stb STBImage]))
 
-(def project
-  (let [[w h]   [540 540]
-        fov     45.0
-        aspect  (/ w h)]
-    (-> (perspective fov aspect 0.1 1000) mat->float-array)))
-
-(def cam-distance 24.0)
-
-(def view
-  (let [position       (v/vec3 0.0 12.0 cam-distance)
-        look-at-target (v/vec3 0.0 12.0 0.0)
-        up             (v/vec3 0.0 1.0 0.0)]
-    (-> (look-at position look-at-target up) mat->float-array)))
-
-(def identity-mat (float-array
-                   [1.0 0.0 0.0 0.0
-                    0.0 1.0 0.0 0.0
-                    0.0 0.0 1.0 0.0
-                    0.0 0.0 0.0 1.0]))
-
-(defn init [{:keys [glfw-window] :as game}]
+(defn init [game]
   (let [ctx nil]
     (gl ctx enable GL_BLEND)
     (gl ctx enable GL_CULL_FACE)
@@ -49,61 +22,11 @@
     (gl ctx enable GL_DEPTH_TEST)
     (STBImage/stbi_set_flip_vertically_on_load false)
     (let [{:keys [w h]} (-> game :config :window-conf)
-          imGuiGlfw     (ImGuiImplGlfw.)
-          imGuiGl3      (ImGuiImplGl3.)
           screen1       (offscreen/prep-offscreen-render ctx w h)]
-      (ImGui/createContext)
-      (doto (ImGui/getIO)
-        (.addConfigFlags ImGuiConfigFlags/DockingEnable)
-        (.setConfigWindowsMoveFromTitleBarOnly  true)
-        (.setFontGlobalScale 1.0))
-      (doto imGuiGlfw
-        (.init glfw-window true)
-        (.setCallbacksChainForAllWindows true))
-      (.init imGuiGl3 "#version 300 es")
       (println "init game")
-      #_{:clj-kondo/ignore [:inline-def]}
-      (def debug-var screen1)
-      (assoc game
-             :imGuiGlfw imGuiGlfw
-             :imGuiGl3 imGuiGl3
-             :screen1 screen1
-             :project project
-             :view view))))
-
-(defn imGuizmoPanel [w h]
-  (ImGuizmo/beginFrame)
-
-  (ImGui/setNextWindowPos 0.0 0.0)
-  (ImGui/setNextWindowSize (float w) (float h))
-  (when (ImGui/begin "imguizmo"
-                     (bit-or ImGuiWindowFlags/NoTitleBar
-                             ImGuiWindowFlags/NoMove
-                             ImGuiWindowFlags/NoBringToFrontOnFocus))
-    (let [manip-x (+ w -150.0)
-          manip-y 16.0]
-      (ImGuizmo/setOrthographic false)
-      (ImGuizmo/enable true)
-      (ImGuizmo/setDrawList)
-      (ImGuizmo/setRect 0.0 0.0 w h)
-      (ImGuizmo/drawGrid view project identity-mat 100)
-      (ImGuizmo/setID 0)
-      (ImGuizmo/viewManipulate view cam-distance manip-x manip-y 128.0 128.0 0x70707070))
-
-    :-)
-
-  (ImGui/end))
-
-(defn imGuiFrame [{:keys [imGuiGl3 imGuiGlfw config]}]
-  (.newFrame imGuiGlfw)
-  (.newFrame imGuiGl3)
-  (ImGui/newFrame)
-  (let [{:keys [w h]} (:window-conf config)]
-    (imGuizmoPanel w h))
-  (let [{:keys [title text]} (:imgui config)]
-    (fps-panel/render! title text))
-  (ImGui/render)
-  (.renderDrawData imGuiGl3 (ImGui/getDrawData)))
+      (-> game
+          (assoc :screen1 screen1)
+          (imgui/init)))))
 
 (defn rendering-zone [game]
   (let [ctx                      nil
@@ -113,7 +36,7 @@
     (gl ctx clearColor 0.02 0.02 0.12 1.0)
     (gl ctx clear (bit-or GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
 
-    (imGuiFrame game)
+    (imgui/frame game)
 
     (gl ctx bindFramebuffer GL_FRAMEBUFFER (:fbo screen1))
     (gl ctx clearColor 0.0 0.0 0.0 0.0)
@@ -130,13 +53,6 @@
                            :scale       (v/vec3 1.0 1.0 1.0)}))
   game)
 
-(defn destroy [{:keys [imGuiGl3 imGuiGlfw]}]
+(defn destroy [game]
   (println "destroy game")
-  (when imGuiGl3 (.shutdown imGuiGl3))
-  (when imGuiGlfw (.shutdown imGuiGlfw))
-  (ImGui/destroyContext))
-
-(comment
-  (require '[com.phronemophobic.viscous :as viscous])
-
-  (viscous/inspect debug-var))
+  (imgui/destroy game))
