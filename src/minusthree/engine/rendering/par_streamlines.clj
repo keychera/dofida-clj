ff2899623cfc11de592104572be62bba49022692
keychera <kyogatama@gmail.com>
2026-02-21 11:54:43 +0900

    resolution

--- src/minusthree/engine/rendering/par_streamlines.clj
+++ src/minusthree/engine/rendering/par_streamlines.clj
@@ -25,10 +25,11 @@
   (str cljgl/version-str
        "
 precision mediump float;
-in vec2 POSITION;
-
+uniform vec2 resolution;
+layout(location=0) in vec2 POSITION;
 void main() {
-  gl_Position = vec4(POSITION, 0.0, 1.0);
+  vec2 p = 2.0 * POSITION * resolution.xy - 1.0;
+  gl_Position = vec4(p, 0.0, 1.0);
 }"))
 
 (def sl-fs
@@ -59,8 +60,8 @@ void main() {
   (let [vertices|| (parsl_position/allocateArray (count verts) arena)]
     (doseq [[i [x y]] (map-indexed vector verts)]
       (let [vert|| (parsl_position/asSlice vertices|| i)]
-        (parsl_position/x vert|| x)
-        (parsl_position/y vert|| y)))
+        (parsl_position/x vert|| (float x))
+        (parsl_position/y vert|| (float y))))
     vertices||))
 
 (defn make-spine-lengths|| ^MemorySegment [^Arena arena ^shorts lengths]
@@ -80,13 +81,18 @@ void main() {
 
 (defn parsl-context|| ^MemorySegment [^Arena arena]
   (let [config||  (doto (parsl_config/allocate arena)
-                    (parsl_config/thickness 0.2))
+                    (parsl_config/thickness 15.0)
+                    (parsl_config/flags par_streamlines_c/PARSL_FLAG_ANNOTATIONS))
         context|| (par_streamlines_c/parsl_create_context config||)]
     (parsl_context/reinterpret context|| arena par_streamlines_c/parsl_destroy_context)))
 
 (defn init [{::arena/keys [game-arena] :as game}]
-  (let [verts         [[0.0 0.0] [0.5 0.3] [1.0 0.0]]
-        spine-length  [3]
+  (let [app-width     600
+        app-height    300
+        resolution    (float-array [(/ 1.0 app-width) (/ 1.0 app-height)])
+        verts         [[50 150] [200 100] [550 200]
+                       [400 200] [400 100]]
+        spine-length  [3 2]
         spine-list||  (make-spine-list|| game-arena verts spine-length)
         context||     (parsl-context|| game-arena)
         mesh||        (par_streamlines_c/parsl_mesh_from_lines context|| spine-list||)
@@ -95,20 +101,16 @@ void main() {
         num-tri       (* 3 (parsl_mesh/num_triangles mesh||))
         tri-indices|| (.asSlice (parsl_mesh/triangle_indices mesh||) 0
                                 (MemoryLayout/sequenceLayout num-tri par_streamlines_c$shared/C_INT))
-        ;; positions  (float-array [-0.5 -0.5,  0.5 -0.5,  0.0  0.5])
-        ;; tri-idxs   (short-array [0 1 2])
         gl-data       (gl-stuff (.asByteBuffer positions||) (.asByteBuffer tri-indices||))]
-    ;; (println [(parsl_mesh/num_vertices mesh||) (parsl_mesh/num_triangles mesh||)])
-    ;; (println [(type (.asIntBuffer (.asByteBuffer tri-indices||)))])
-    ;; (println (vec (.toArray tri-indices|| par_streamlines_c$shared/C_INT)))
-    ;; (println (vec (.toArray positions|| par_streamlines_c$shared/C_FLOAT)))
     (assoc game
+           :resolution resolution
            :num-tri num-tri
            :vao (:vao gl-data)
-           :program (-> gl-data :program-info :program))))
+           :program-info (:program-info gl-data))))
 
-(defn render [{:keys [vao :program num-tri]}]
-  (GL45/glUseProgram program)
+(defn render [{:keys [vao program-info num-tri resolution]}]
+  (GL45/glUseProgram (:program program-info))
+  (cljgl/set-uniform program-info :resolution resolution)
   (GL45/glBindVertexArray vao)
   (GL45/glDrawElements GL45/GL_TRIANGLES num-tri GL45/GL_UNSIGNED_INT 0))
 
