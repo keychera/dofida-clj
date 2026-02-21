5c913ef472361c9c6874e1820b46ecea368d5f10
keychera <kyogatama@gmail.com>
2026-02-21 17:41:14 +0900

    circle

--- src/minusthree/engine/rendering/par_streamlines.clj
+++ src/minusthree/engine/rendering/par_streamlines.clj
@@ -53,9 +53,12 @@ out float spine_length;
 
 void main() {
   vec2 p = 2.0 * POSITION * resolution.xy - 1.0;
-  gl_Position = vec4(p, 0.0, 1.0);
   annotation = ANNOTATION;
   spine_length = SPINE_LEN;
+  vec2 spine_to_edge = annotation.zw;
+  float wave = 0.5 + 0.5 * sin(10.0 * 6.28318 * annotation.x);
+  p = p + spine_to_edge * 0.01 * wave;
+  gl_Position = vec4(p, 0.0, 1.0);
 }"))
 
 (def sl-fs
@@ -115,9 +118,11 @@ void main() {
     (GL45/glBindBuffer GL45/GL_ARRAY_BUFFER spine-len-buf)
     (GL45/glBufferSubData GL45/GL_ARRAY_BUFFER 0 (.asByteBuffer mesh-sp-lens||))))
 
+(defn vert [x y] {:x x :y y})
+
 (defn make-vertices|| ^MemorySegment [^Arena arena verts]
   (let [vertices|| (parsl_position/allocateArray (count verts) arena)]
-    (doseq [[i [x y]] (map-indexed vector verts)]
+    (doseq [[i {:keys [x y]}] (map-indexed vector verts)]
       (let [vert|| (parsl_position/asSlice vertices|| i)]
         (parsl_position/x vert|| (float x))
         (parsl_position/y vert|| (float y))))
@@ -139,25 +144,37 @@ void main() {
 
 (defn parsl-context|| ^MemorySegment [^Arena arena]
   (let [config||  (doto (parsl_config/allocate arena)
-                    (parsl_config/thickness 15.0)
+                    (parsl_config/thickness 4.0)
                     (parsl_config/flags (bit-or (parsl/PARSL_FLAG_ANNOTATIONS)
                                                 (parsl/PARSL_FLAG_SPINE_LENGTHS)))
                     (parsl_config/u_mode (parsl/PAR_U_MODE_DISTANCE)))
         context|| (parsl/parsl_create_context config||)]
     (parsl_context/reinterpret context|| arena parsl/parsl_destroy_context)))
 
+(defn zero-if-small [v]
+  (if (< (abs v) 1e-5) 0 v))
+
+(defn make-circle [num-of-vert x y r]
+  (let [two-pi (* 2 Math/PI)
+        angle-shift (/ two-pi num-of-vert)]
+    (->> (range (inc num-of-vert))
+         (map (fn [i] (let [theta (* i angle-shift)]
+                        (vert (zero-if-small (+ x (* r (Math/cos theta))))
+                              (zero-if-small (+ y (* r (Math/sin theta)))))))))))
+
 (defn init [{:keys [::arena/game-arena] :as game}]
   (let [app-width       600
-        app-height      300
+        app-height      600
         resolution      (float-array [(/ 1.0 app-width) (/ 1.0 app-height)])
-        verts           [[50 150] [200 100] [550 200]
-                         [400 200] [400 100]]
-        spine-lengths   [3 2]
+        circle-num       42
+        verts           (flatten [(make-circle circle-num 300.0 300.0 250.0)
+                                  [(vert 175 175) (vert 300 300) (vert 250 50)]])
         num-vert        (count verts)
+        spine-lengths   [(inc circle-num) 3]
         verts||         (make-vertices|| game-arena verts)
         num-spines      (count spine-lengths)
         spine-lengths|| (make-spine-lengths|| game-arena spine-lengths)
-        spine-list||    (make-spine-list|| game-arena num-vert verts|| num-spines spine-lengths||)
+        spine-list||    (make-spine-list|| game-arena num-vert verts|| num-spines spine-lengths|| false)
         context||       (parsl-context|| game-arena)
         mesh||          (parsl/parsl_mesh_from_lines context|| spine-list||)
         positions||     (.asSlice (parsl_mesh/positions mesh||) 0
@@ -186,13 +203,13 @@ void main() {
 (defn render [{:keys [vao program-info num-tri resolution
                       verts|| context|| spine-list|| ::buffers]
                tt ::time/total}]
-  (let [t-factor (* Math/PI tt 1e-3)]
-    (doto ^MemorySegment verts||
-      (-> (parsl_position/asSlice 1) (parsl_position/y (+ 150 (* 100 (Math/sin t-factor)))))
-      (-> (parsl_position/asSlice 3) (parsl_position/x (+ 400 (* 50 (Math/cos t-factor)))))
-      (-> (parsl_position/asSlice 3) (parsl_position/y (+ 150 (* 50 (Math/sin t-factor)))))
-      (-> (parsl_position/asSlice 4) (parsl_position/x (- 400 (* 50 (Math/cos t-factor)))))
-      (-> (parsl_position/asSlice 4) (parsl_position/y (- 150 (* 50 (Math/sin t-factor)))))))
+  (let [t-factor (* Math/PI tt 5e-6)]
+      (doto ^MemorySegment verts||
+        (-> (parsl_position/asSlice 0) (parsl_position/x (+ 250.0 (* 50 (Math/cos t-factor)))))
+        #_#_#_#_(-> (parsl_position/asSlice 3) (parsl_position/x (+ 400 (* 50 (Math/cos t-factor)))))
+        (-> (parsl_position/asSlice 3) (parsl_position/y (+ 150 (* 50 (Math/sin t-factor)))))
+        (-> (parsl_position/asSlice 4) (parsl_position/x (- 400 (* 50 (Math/cos t-factor)))))
+        (-> (parsl_position/asSlice 4) (parsl_position/y (- 150 (* 50 (Math/sin t-factor)))))))
   (let [mesh|| (parsl/parsl_mesh_from_lines context|| spine-list||)]
     (GL45/glUseProgram (:program program-info))
     (gl-update-buffers mesh|| buffers)
