29f3ff0fca79da57785ae58300c23397c19ea614
keychera <kyogatama@gmail.com>
2026-02-22 23:52:50 +0900

    ia and game-arena, now we are dealing with lifetimes

--- src/minusthree/engine/rendering/playground.clj
+++ src/minusthree/engine/rendering/playground.clj
@@ -3,8 +3,6 @@
    [com.phronemophobic.viscous :as viscous]
    [minusthree.engine.ffm.arena :as arena]
    [minusthree.engine.loader :as loader]
-   [minusthree.engine.rendering.par-streamlines :refer [parsl-context||]]
-   [minusthree.engine.time :as time]
    [minusthree.gl.cljgl :as cljgl]
    [minusthree.gl.gl-magic :as gl-magic]
    [minusthree.gl.shader :as shader]
@@ -63,80 +61,75 @@ void main() {
   (doto (b2Vec2/allocate arena) (b2Vec2/x x) (b2Vec2/y y)))
 
 (defn init [{:keys [::arena/game-arena] :as game}]
-  (tvg/tvg_engine_init 4)
-  (let [WIDTH        400
-        HEIGHT       400
+  (with-open [ia #_init-arena (Arena/ofConfined)]
+    (tvg/tvg_engine_init 4)
+    (let [WIDTH        400
+          HEIGHT       400
 
-        ;; box2d https://box2d.org/documentation/hello.html
-        ;; la = local arena
-        b2d-world-id|| (with-open [la (Arena/ofConfined)]
-                         (let [b2-vec2||   (b2dvec2 la 0.0 -10.0)
-                               world-def|| (doto (b2d/b2DefaultWorldDef la)
+          ;; box2d https://box2d.org/documentation/hello.html
+          ;; some use init-arena because, following box2d tutorial, the values are copied and not needed anymore
+          ;; therefore safe to dealloc, which is done automatically after exiting ia with-open scope
+          b2d-world-id|| (let [b2-vec2||   (b2dvec2 ia 0.0 -10.0)
+                               world-def|| (doto (b2d/b2DefaultWorldDef ia)
                                              (b2WorldDef/gravity b2-vec2||))]
-                           (b2d/b2CreateWorld game-arena world-def||)))
+                           (b2d/b2CreateWorld game-arena world-def||))
 
-        ground-id||    (with-open [la (Arena/ofConfined)]
-                         (let [b2-vec2||     (b2dvec2 la 0.0 -20.0)
-                               b2-body-def|| (doto (b2d/b2DefaultBodyDef la)
+          ground-id||    (let [b2-vec2||     (b2dvec2 ia 0.0 -20.0)
+                               b2-body-def|| (doto (b2d/b2DefaultBodyDef ia)
                                                (b2BodyDef/position b2-vec2||))]
-                           (b2d/b2CreateBody game-arena b2d-world-id|| b2-body-def||)))
+                           (b2d/b2CreateBody game-arena b2d-world-id|| b2-body-def||))
+          _               (let [ground-box||       (b2d/b2MakeBox ia 50.0 10.0)
+                                ground-shape-def|| (b2d/b2DefaultShapeDef ia)]
+                            (b2d/b2CreatePolygonShape game-arena ground-id|| ground-shape-def|| ground-box||))
 
-        _               (with-open [la (Arena/ofConfined)]
-                          (let [ground-box||       (b2d/b2MakeBox la 50.0 10.0)
-                                ground-shape-def|| (b2d/b2DefaultShapeDef la)]
-                            (b2d/b2CreatePolygonShape game-arena ground-id|| ground-shape-def|| ground-box||)))
-
-        body-id||      (with-open [la (Arena/ofConfined)]
-                         (let [b2-body-def|| (doto (b2d/b2DefaultBodyDef la)
+          body-id||      (let [b2-body-def|| (doto (b2d/b2DefaultBodyDef ia)
                                                (b2BodyDef/type (b2d/b2_dynamicBody))
-                                               (b2BodyDef/position (b2dvec2 la 0.0 4.0)))]
-                           (b2d/b2CreateBody game-arena b2d-world-id|| b2-body-def||)))
-
-        _              (with-open [la (Arena/ofConfined)]
-                         (let [dynamic-box|| (b2d/b2MakeBox la 1.0 1.0)
-                               shape-def||   (doto (b2d/b2DefaultShapeDef la)
+                                               (b2BodyDef/position (b2dvec2 ia 0.0 4.0)))]
+                           (b2d/b2CreateBody game-arena b2d-world-id|| b2-body-def||))
+          _              (let [dynamic-box|| (b2d/b2MakeBox ia 1.0 1.0)
+                               shape-def||   (doto (b2d/b2DefaultShapeDef ia)
                                                (b2ShapeDef/density 1.0)
                                                (-> (b2ShapeDef/material) (b2SurfaceMaterial/friction 0.3)))]
-                           (b2d/b2CreatePolygonShape game-arena body-id|| shape-def|| dynamic-box||)))
+                           (b2d/b2CreatePolygonShape game-arena body-id|| shape-def|| dynamic-box||))
 
-        ;; thorvg https://www.thorvg.org/native-tutorial
-        buffer||       (.allocate ^Arena game-arena (MemoryLayout/sequenceLayout (* WIDTH HEIGHT) tvg/C_INT))
-        canvas||       (doto (tvg/tvg_swcanvas_create (tvg/TVG_ENGINE_OPTION_DEFAULT))
-                         (tvg/tvg_swcanvas_set_target buffer|| WIDTH WIDTH HEIGHT (tvg/TVG_COLORSPACE_ABGR8888)))
-        rect||         (doto (tvg/tvg_shape_new)
-                         (tvg/tvg_shape_append_rect #_x-y 100 100 #_w-h 100 100 #_rx-ry 15 15 #_cw? false)
-                         ;; unsure why the color is dimmed rn
-                         (tvg/tvg_shape_set_fill_color #_rgba 127 0 0 127))  ;; not 255 since java use unsigned bytes  -128 .. 127 
-        _              (doto canvas||
-                         (tvg/tvg_canvas_add rect||)
-                         (tvg/tvg_canvas_update)
-                         (tvg/tvg_canvas_draw #_clear true)
-                         (tvg/tvg_canvas_sync))
-        thor-tex       (texture/create-texture (.asByteBuffer buffer||) WIDTH HEIGHT)
+          ;; thorvg https://www.thorvg.org/native-tutorial
+          buffer||       (.allocate ^Arena game-arena (MemoryLayout/sequenceLayout (* WIDTH HEIGHT) tvg/C_INT))
+          canvas||       (doto (tvg/tvg_swcanvas_create (tvg/TVG_ENGINE_OPTION_DEFAULT))
+                           (tvg/tvg_swcanvas_set_target buffer|| WIDTH WIDTH HEIGHT (tvg/TVG_COLORSPACE_ABGR8888)))
+          rect||         (doto (tvg/tvg_shape_new)
+                           (tvg/tvg_shape_append_rect #_x-y 100 100 #_w-h 100 100 #_rx-ry 15 15 #_cw? false)
+                           ;; unsure why the color is dimmed rn
+                           (tvg/tvg_shape_set_fill_color #_rgba 127 0 0 127))  ;; not 255 since java use unsigned bytes  -128 .. 127 
+          _              (doto canvas||
+                           (tvg/tvg_canvas_add rect||)
+                           (tvg/tvg_canvas_update)
+                           (tvg/tvg_canvas_draw #_clear true)
+                           (tvg/tvg_canvas_sync))
+          thor-tex       (texture/create-texture (.asByteBuffer buffer||) WIDTH HEIGHT)
 
-        ;; gl stuff  
-        program-info   (cljgl/create-program-info-from-source fbo-vs fbo-fs)
-        gl-summons     (gl-magic/cast-spell
-                        [{:bind-vao "thor"}
-                         {:buffer-data plane3d-vertices :buffer-type GL45/GL_ARRAY_BUFFER}
-                         {:point-attr :a_pos :use-shader program-info :count 3 :component-type GL45/GL_FLOAT}
-                         {:buffer-data plane3d-uvs :buffer-type GL45/GL_ARRAY_BUFFER}
-                         {:point-attr :a_uv :use-shader program-info :count 2 :component-type GL45/GL_FLOAT}
-                         {:unbind-vao true}])
-        vao          (-> gl-summons ::gl-magic/data ::gl-magic/vao (get "thor"))
-        id->buffer   (-> gl-summons ::gl-magic/data ::shader/buffer)]
-    (println "box2d index:" (b2WorldId/index1 b2d-world-id||) ", generation:" (b2WorldId/generation b2d-world-id||))
-    (println "playground ready!")
-    #_{:clj-kondo/ignore [:inline-def]}
-    (def debug-var (vec (.toArray buffer|| tvg/C_INT)))
-    (assoc game
-           ;; box2d
-           ::b2d-world-id|| b2d-world-id|| ::body-id|| body-id||
-           ;; thorvg
-           ::buffer|| buffer|| ::canvas|| canvas|| ::rect|| rect||
-           ;; gl
-           ::program-info program-info ::vao vao ::thor-tex thor-tex ::id->buffer id->buffer
-           ::WIDTH WIDTH ::HEIGHT HEIGHT)))
+          ;; gl stuff  
+          program-info   (cljgl/create-program-info-from-source fbo-vs fbo-fs)
+          gl-summons     (gl-magic/cast-spell
+                          [{:bind-vao "thor"}
+                           {:buffer-data plane3d-vertices :buffer-type GL45/GL_ARRAY_BUFFER}
+                           {:point-attr :a_pos :use-shader program-info :count 3 :component-type GL45/GL_FLOAT}
+                           {:buffer-data plane3d-uvs :buffer-type GL45/GL_ARRAY_BUFFER}
+                           {:point-attr :a_uv :use-shader program-info :count 2 :component-type GL45/GL_FLOAT}
+                           {:unbind-vao true}])
+          vao          (-> gl-summons ::gl-magic/data ::gl-magic/vao (get "thor"))
+          id->buffer   (-> gl-summons ::gl-magic/data ::shader/buffer)]
+      (println "box2d index:" (b2WorldId/index1 b2d-world-id||) ", generation:" (b2WorldId/generation b2d-world-id||))
+      (println "playground ready!")
+      #_{:clj-kondo/ignore [:inline-def]}
+      (def debug-var (vec (.toArray buffer|| tvg/C_INT)))
+      (assoc game
+             ;; box2d
+             ::b2d-world-id|| b2d-world-id|| ::body-id|| body-id||
+             ;; thorvg
+             ::buffer|| buffer|| ::canvas|| canvas|| ::rect|| rect||
+             ;; gl
+             ::program-info program-info ::vao vao ::thor-tex thor-tex ::id->buffer id->buffer
+             ::WIDTH WIDTH ::HEIGHT HEIGHT))))
 
 (defn b2Rot->angle [segment]
   ;; https://github.com/manuelbl/JavaDoesUSB/tree/main/java-does-usb/jextract
@@ -199,11 +192,6 @@ void main() {
                          (bit-shift-right rgba 16)
                          (bit-shift-right rgba 24)])))
    debug-var)
-
-  (loader/load-libs "par_streamlines")
-  (with-open [arena (Arena/ofConfined)]
-    (let [a (parsl-context|| arena)]
-      (println (type a))))
 
   (loader/load-libs "box2dd")
   (type b2d)
